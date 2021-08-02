2d53165c0da6428208e3f3137194088fde02f911
Or Hasson <hassonor@gmail.com>
2021-08-02 15:23:41 +0300

    Add Image Upload and Static styling

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- projects/models.py
+++ projects/models.py
@@ -2,10 +2,10 @@ from django.db import models
 import uuid
 
 
-
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)

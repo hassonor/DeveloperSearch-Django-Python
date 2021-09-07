8fded1a98053d427be7b5479cb253e6bf6425aca
Or Hasson <hassonor@gmail.com>
2021-09-07 15:36:11 +0300

    Adding Sending Email After Register

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -119,6 +119,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'noreplay.hasson@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
+from django.core.mail import send_mail
+from django.conf import settings
 from django.dispatch import receiver
 
 from users.models import Profile
@@ -16,16 +18,16 @@ def created_profile(sender, instance, created, **kwargs):
             name=user.first_name,
         )
 
-def created_profile(sender, instance, created, **kwargs):
-    print('Profile signal triggered')
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email=user.email,
-            name=user.first_name,
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you joined our community!'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
+
 
 def update_user(sender, instance, created, **kwargs):
     profile = instance

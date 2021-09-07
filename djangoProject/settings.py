13c604f00617211895c582af207a464875361d7f
Or Hasson <hassonor@gmail.com>
2021-09-07 21:19:53 +0300

    Adding Password Reset Logic

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -124,7 +124,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noreplay.hasson@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'oupiudeccvcmpqpr'
 
 
 # Static files (CSS, JavaScript, Images)
